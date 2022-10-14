e61aec3d2af85ac2a456ed215b8a3c63abb48b2f
JeongJinGan <rksaudwls123@naver.com>
2022-10-14 14:21:35 +0900

    update urls

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,5 @@ urlpatterns = [
     path("", views.index, name="index"),
     path("signup/", views.signup, name="signup"),
     path("login/", views.login, name="login"),
-    path("<int:pk>/", views.detail, name="detail"),
+    path("detail/<int:pk>/", views.detail, name="detail"),
 ]
